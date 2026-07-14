f0e171c3814b71ff3da357a67b6c882b2d94f561
rajputnitu82 <rajputnitu82@gmail.com>
2026-07-14 10:53:51 +0530

    Practical1.java

---
+++ Practical1.java
@@ -0,0 +1,46 @@
+public class Practical1{
+    
+    public static void main(String[]args){
+        System.out.println("Hello World!");
+        System.out.println();
+
+        int age = 20;
+        double salary = 35000.75;
+        char grade ='A';
+        boolean isStudent = true;
+        String name = "Rahul";
+
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + age);
+        System.out.println("Salary:" + salary);
+        System.out.println("Grade:" + grade);
+        System.out.println("Student:" + isStudent);
+
+
+
+        System.out.println();
+
+
+        int number = 50;
+        double result = number;
+
+        System.out.println("implicit Type Casting");
+        System.out.println("integer value:" + number);
+        System.out.println("Converted to Double:" + result);
+
+        System.out.println();
+
+        //Explicit Type Casting(manual)
+        double marks = 89.75;
+        int totalMarks = (int) marks;
+
+        System.out.println("Explicit Type Casting");
+        System.out.println("Double Value:" + marks);
+        System.out.println("Converted to integer:" + totalMarks);
+    
+    int number1=10;
+    int number2=20;
+    int add=number1+number2;
+    System.out.println("Addition of " +number1+ " And " +number2+ " is: "+add);
+    }
+}
